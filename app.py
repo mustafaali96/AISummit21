38455373811cac54961e746a51dbc921e7e54414
Mustafa Ali <engr.mustafa.ali.mir@gmail.com>
2021-11-03 10:21:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pymongo
 import pandas as pd
-from pandas.io.json import json_normalize
+# from pandas.io.json import json_normalize
 
 # Initialize connection.
 # client = pymongo.MongoClient(**st.secrets["mongo"])
@@ -12,16 +12,18 @@ db = client.customer_complaints
 cursor_items = db.complaints.find()
 # items = list(cursor_items)
 # df = pd.DataFrame(items)
-df = json_normalize(list(cursor_items))
+# df = json_normalize(list(cursor_items))
 
 st.title('AI Summit 21 Demo!')
 # st.header('This is a header')
 
-st.write("Total Records", cursor_items.count())
-st.write("Timely Done", db.complaints.count_documents({ "timely": "Yes"}))
-st.write("Timely Not Done", db.complaints.count_documents({ "timely": {"$ne":"Yes"}}))
+st.write("Total Records", cursor_items.count(), "Timely Done", db.complaints.count_documents({ "timely": "Yes"}), "Timely Not Done", db.complaints.count_documents({ "timely": {"$ne":"Yes"}}))
 
-# for item in items:
-#     st.write(f"{item['product']} has a :{item['complaint_what_happened']}:")
+
+all_records = []
+for item in cursor_items:
+    # st.write(f"{item['product']} has a :{item['complaint_what_happened']}:")
+    all_records.append(item)
+df = pd.DataFrame(all_records)
 
 st.write(df)
