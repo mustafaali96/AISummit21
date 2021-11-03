09c29c2cfdce0ce9f334798b1547b519842ed477
Mustafa Ali <engr.mustafa.ali.mir@gmail.com>
2021-11-03 09:53:11 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pymongo
+import pandas as pd
 
 # Initialize connection.
 # client = pymongo.MongoClient(**st.secrets["mongo"])
@@ -9,9 +10,14 @@ client = pymongo.MongoClient("mongodb+srv://mustafa:66762532mufa@aisummit21.5l1c
 db = client.customer_complaints
 items = db.complaints.find()
 items = list(items)
+df = pd.DataFrame(items)
+
+total_records = items.count()
+st.write("Total Records Found", items.count())
+st.write("Timely Done", db.complaints.count_documents({ "timely": "Yes"}))
+st.write("Timely Not Done", db.complaints.count_documents({ "timely": {"$ne":"Yes"}}))
 
 for item in items:
     st.write(f"{item['product']} has a :{item['complaint_what_happened']}:")
 
-st.write("Hello world")
 # st.write(items)
